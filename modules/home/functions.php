7e032c59fd92ece4da6a163696181dc4e5880734
tmsvietnam <42080593+tmsvietnam@users.noreply.github.com>
2018-08-08 16:41:54 +0700

    up module home

---
+++ modules/home/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VIETNAM DIGITAL TRADING TECHNOLOGY  (contact@tmsvietnam.com)
+ * @Copyright (C) 2014 VIETNAM DIGITAL TRADING TECHNOLOGY . All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate 08-08-2018 08:08
+ */
+
+if (! defined('NV_SYSTEM')) {
+    die('Stop!!!');
+}
+
+define('NV_IS_MOD_HOME', true);
